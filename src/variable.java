3019f12bb1fb69a8b5cb520c65eebb54e7e16b3e
Preeti Bhardwaj <preeti1401bhardwaj@gmail.com>
2025-12-11 18:42:15 +0530

    variable

---
+++ src/variable.java
@@ -0,0 +1,20 @@
+public class variable {
+    public static void main(String args[]){
+        int a= 12;
+        long value=1296544405;
+        float pie= 3.14f;
+        double num= 2374937.97459479554;
+        byte cash=123;
+        short series=4958;
+        boolean option= true;
+        char sign= 'a';
+        System.out.println("a:"+a);
+        System.out.println("value"+value);
+        System.out.println("num"+num);
+        System.out.println("cash"+cash);
+        System.out.println("pie"+pie);
+        System.out.println("sign"+sign);
+        System.out.println("option"+option);
+        System.out.println("series"+series);
+    }
+}
