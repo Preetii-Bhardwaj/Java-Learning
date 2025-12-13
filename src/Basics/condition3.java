034c83d7288923f58c8af35875a7755e19fe7f4c
Preeti Bhardwaj <preeti1401bhardwaj@gmail.com>
2025-12-13 21:34:43 +0530

    condition3

--- src/Basics/condition3.java
+++ src/Basics/condition3.java
@@ -5,7 +5,7 @@ public class condition3 {
         Scanner sc=new Scanner(System.in);
         System.out.print("Enter number 1:");
         int num1=sc.nextInt();
-         System.out.print("Enter number 1:");
+         System.out.print("Enter number 2:");
         int num2=sc.nextInt();
         if(num1==num2){
             System.out.println("num1 is equal to num2:"+num1);
