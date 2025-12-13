19a1d534ea8c8e7556f594c954773273dde95009
Preeti Bhardwaj <preeti1401bhardwaj@gmail.com>
2025-12-13 20:42:25 +0530

    condition1

---
+++ src/Basics/condition1.java
@@ -0,0 +1,16 @@
+package Basics;
+import java.util.*;
+public class condition1 {
+  public static void main(String args[]){
+    Scanner sc= new Scanner(System.in);
+    System.out.println("Enter your age:");
+    int age=sc.nextInt();
+    if(age>=18){
+        System.out.println("Adult");
+    }
+    else{
+         System.out.println("Not Adult");
+    }
+
+  }
+}
