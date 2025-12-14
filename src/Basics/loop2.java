2a59943a7b1f78741c447659ea5201d72da78067
Preeti Bhardwaj <preeti1401bhardwaj@gmail.com>
2025-12-14 21:58:49 +0530

    while loop

---
+++ src/Basics/loop2.java
@@ -0,0 +1,14 @@
+package Basics;
+import java.util.*;
+public class loop2 {
+    public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter your number:");
+        int a=sc.nextInt();
+        int i=0;
+        while(i<a){
+            System.out.println(i+ " ");
+           i++;
+        } 
+    }
+}
