34a6e9a353a35aa67a3e920e2e5998bbb68e5945
Preeti Bhardwaj <preeti1401bhardwaj@gmail.com>
2025-12-14 15:08:19 +0530

    Calendar in French

---
+++ src/Basics/calendar.java
@@ -0,0 +1,36 @@
+package Basics;
+import java.util.*;
+public class calendar {
+   public static void main(String args[]){
+    Scanner sc= new Scanner(System.in);
+    System.out.print("Entrez votre choix:");
+    int choice=sc.nextInt();
+    switch(choice){
+        case 1:System.out.println("Janvier");
+        break;
+        case 2:System.out.println("février");
+        break;
+        case 3:System.out.println("Mars");
+        break;
+        case 4:System.out.println("Avril");
+        break;
+        case 5:System.out.println("Mai");
+        break;
+        case 6:System.out.println("Juin");
+        break;
+        case 7:System.out.println("Juillet");
+        break;
+        case 8:System.out.println("août");
+        break;
+        case 9:System.out.println("septembre");
+        break;
+        case 10:System.out.println("Octobre");
+        break;
+        case 11:System.out.println("Novembre");
+        break;
+        case 12:System.out.println("Décembre");
+        break;
+        default:System.out.println("essayer à nouveau");
+    }
+   } 
+}
