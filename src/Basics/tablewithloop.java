4fa5f0a965770aae8c017c57ac2b4feba5f7c5f6
Preeti Bhardwaj <preeti1401bhardwaj@gmail.com>
2025-12-14 23:16:02 +0530

    table with loops

---
+++ src/Basics/tablewithloop.java
@@ -0,0 +1,12 @@
+package Basics;
+import java.util.*;
+public class tablewithloop {
+  public static void main(String args[]){
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter the number:");
+    int n=sc.nextInt();
+    for(int i=1;i<11;i++){
+        System.out.println(n+"*"+i+"="+n*i);
+    }
+  }  
+}
