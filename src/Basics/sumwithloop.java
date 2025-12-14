aafcdeb79ac7ba16c9c7ef649ba09725a3b7fbc9
Preeti Bhardwaj <preeti1401bhardwaj@gmail.com>
2025-12-14 23:01:06 +0530

    sum of natural numbers

--- src/Basics/sumwithloop.java
+++ src/Basics/sumwithloop.java
@@ -6,7 +6,7 @@ public class sumwithloop{
         System.out.print("Enter the number:");
         int n= sc.nextInt();
         int sum=0;
-        for(int i=0;i<=n;i++){
+        for(int i=1;i<=n;i++){
           
            
            sum=sum+i;
