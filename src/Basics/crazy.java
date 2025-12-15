68d1d2c658137de38447b40a52db4536a0a21e6d
Preeti Bhardwaj <preeti1401bhardwaj@gmail.com>
2025-12-15 14:05:01 +0530

    minor changes with code

---
+++ src/Basics/crazy.java
@@ -0,0 +1,22 @@
+package Basics;
+
+public class crazy {
+    public static void main(String args[]){
+        int n=12;
+     /*  for(int i=0;i<n;i++){
+           System.out.println("HELLO WORLD");                //  correct output
+       } */
+
+
+
+         /*for(int i=0;;i++){                               // end less loop without condition
+            System.out.println("HELLO WORLD");
+        }    */ 
+
+
+
+         /*  for(int i=0;i<n;){                                 //without increment end less loop
+            System.out.println("HELLO WORLD");
+        }  */
+    }
+}
